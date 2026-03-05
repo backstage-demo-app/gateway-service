ad287aee42c3fb0a3adcc143713e6c1bab2675ce
Scaffolder <scaffolder@backstage.io>
2026-03-05 20:22:21 +0300

    initial commit

---
+++ src/main/java/com/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.demo.gatewayservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String, String> hello() {
+        return Map.of(
+            "service", "gateway-service",
+            "message", "Hello from gateway-service!",
+            "status", "running"
+        );
+    }
+}
